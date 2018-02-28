5c717aee08925f7b72ab6ad887df3293f1ab0ef8
LivingAutoMode <livingautomode@outlook.com>
2018-02-28 20:20:32 +0000

    Keep your long brush strokes indented.

--- commands/test.js
+++ commands/test.js
@@ -7,7 +7,14 @@ module.exports = {
     console.log(cmd.author.iconUrl)
     superagent.get(cmd.author.iconUrl).buffer(true).end((err, resp) => {
       console.log(resp.request)
-      cb({img:{buffer: resp.body, name:'avatar.' + cmd.author.iconUrl.split('.')[cmd.author.iconUrl.split('.').length-1].split('?')[0]}})
+      cb(
+		  {
+			img:{
+				buffer: resp.body,
+			  	name:'avatar.' + cmd.author.iconUrl.split('.')[cmd.author.iconUrl.split('.').length-1].split('?')[0]
+		  	}
+	  	  }
+		)
     })
   }
 }
